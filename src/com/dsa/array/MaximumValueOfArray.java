54a1dc4111785a335089d4389fd0d07d8107e843
deep-pansara <pansaradeep@gmail.com>
2024-06-04 09:55:08 +0530

    DSA array

---
+++ src/com/dsa/array/MaximumValueOfArray.java
@@ -0,0 +1,28 @@
+package com.dsa.array;
+
+public class MaximumValueOfArray {
+
+
+    public static void main(String[] args) {
+        int arr[] = {3, 5, 1, 2, 9, 23, 50, 2, 1, 5, 7};
+
+
+        int max = Integer.MIN_VALUE;
+        int min = arr[0];
+        int second_max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+
+
+            System.out.println("max value -  " + max);
+            System.out.println("min value -  " + min);
+
+
+        }
+    }
